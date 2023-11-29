1c3690a5af4a12db3ccab7ace0a431f2326f5ee1
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-11-29 16:04:19 +0100

    Single card Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
+  {"src": "/img/helmet-1.png", matched: false},
+  {"src": "/img/potion-1.png", matched: false},
+  {"src": "/img/ring-1.png", matched: false},
+  {"src": "/img/scroll-1.png", matched: false},
+  {"src": "/img/shield-1.png", matched: false},
+  {"src": "/img/sword-1.png", matched: false}
 ]
 
 function App() {
 
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-
+  const [choiseOne, setChoiseOne] = useState(null);
+  const [choiseTwo, setChoiseTwo] = useState(null);
   //Shuffle Cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -27,6 +28,40 @@ function App() {
   }
 
 
+  //handle choise
+  const handleChoise = (card) =>{
+      choiseOne ? setChoiseTwo(card) : setChoiseOne(card)
+  }
+
+  // compare 2 selected cards
+useEffect(() => {
+  if(choiseOne && choiseTwo){
+
+    if(choiseOne.src === choiseTwo.src){
+      setCards(prevCards => {
+        return prevCards.map(card =>{
+            if(card.src === choiseOne.src){
+              return {...card, matched: true};
+            }else{
+              return card;
+            }
+        })
+      })
+      resetTurn()
+    }else {
+      resetTurn()
+    }
+  }
+},[choiseOne, choiseTwo])
+
+  //reset choise & increment turn
+
+  const resetTurn = () =>{
+    setChoiseOne(null)
+    setChoiseTwo(null)
+    setTurns(prevTurns => prevTurns +1 )
+  }
+
   return (
     <div className="App">
     <h1>Enchanted Matches</h1>
@@ -34,7 +69,7 @@ function App() {
 
     <div className='card-grid'>
       {cards.map(card =>(
-       <SingleCard key={card.id} card ={card}/>
+       <SingleCard key={card.id} card ={card} handleChoise={handleChoise}/>
       ))}
     </div>
     </div>
